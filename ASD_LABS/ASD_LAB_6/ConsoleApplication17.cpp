4b7b2377c60147d689c9d90204af5429c51c2442
Maksym <71383659+Maksolo27@users.noreply.github.com>
2021-03-01 18:17:02 +0200

    Add files via upload

--- ASD_LABS/ASD_LAB_6/ConsoleApplication17.cpp
+++ ASD_LABS/ASD_LAB_6/ConsoleApplication17.cpp
@@ -1,23 +1,24 @@
 ﻿#include <stdio.h>
 #include<math.h>
-#define x 0.1
-
-
+#define x 0.9
 
 int main()
 {
-	float F = (x - 1) / (x + 1);
-	float sum = F;
-	int input;
-	printf_s("Please, input your integer number\n");
-	scanf_s("%d", &input);
-	for (int i = 2; i <= input; i++) {
-		F = F * (2 * i - 1) * powf((x - 1), 2) / ((2 * i + 1) * powf((x + 1), 2));
+	float sum = (x - 1)/(x + 1);
+	float F = (x - 1)/(x + 1);
+	int num;
+	printf_s("Enter number: ");
+	scanf_s("%d", &num);
+	printf_s("F%d = %.8lf\n", 1, F * -1);
+	for (int i = 2; i <= num; i++) {
+		F = F * (2 * i - 1) * (pow(x - 1, 2)) / ((2*i+1)*(pow(x+1, 2)));
+		printf_s("F%d = %.8lf\n", i, F * -1);
 		sum += F;
+	
 	}
-	printf_s("Sum of range equal: ( %f )\n", sum);
+
+	printf_s("Sum = %.8lf\n", sum*-1);
 }
 
-//F* (2 * i - 1)* powf((x - 1), 2) / ((2 * i + 1) * powf((x + 1), 2))
 
 
