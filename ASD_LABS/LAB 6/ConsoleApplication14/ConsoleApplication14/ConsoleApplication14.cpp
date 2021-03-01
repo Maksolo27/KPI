0d4096fc59f45518106fcc27f553c814ea7be606
Maksym <71383659+Maksolo27@users.noreply.github.com>
2021-03-01 19:58:33 +0200

    Add files via upload

---
+++ ASD_LABS/LAB 6/ConsoleApplication14/ConsoleApplication14/ConsoleApplication14.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <math.h>
+#define x 0.5
+
+
+float RecurseSum(int length, float prevF, float sum, int i) {
+	i++;
+	prevF = prevF * (2 * i - 1)* pow((x-1), 2) / ((2 * i + 1) * pow((x + 1), 2));
+	sum += prevF;
+	if (length + 1 != i) {
+		sum = RecurseSum(length, prevF, sum, i);
+	}
+	return sum;
+}
+int main()
+{
+	float F = (x-1)/(x+1);
+	int input;
+	float sum = (x-1)/(x+1);
+	printf_s("Enter your number: \n");
+	scanf_s("%d", &input);
+	sum = RecurseSum(input, F, sum, 1);
+	printf_s("Sum =  %f \n", sum * -1);
+}
---
+++ ASD_LABS/LAB 6/ConsoleApplication17/ConsoleApplication17/ConsoleApplication17.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include<math.h>
+#define x 0.9
+
+int main()
+{
+	float sum = (x - 1)/(x + 1);
+	float F = (x - 1)/(x + 1);
+	int num;
+	printf_s("Enter number: ");
+	scanf_s("%d", &num);
+	printf_s("F%d = %.8lf\n", 1, F * -1);
+	for (int i = 2; i <= num; i++) {
+		F = F * (2 * i - 1) * (pow(x - 1, 2)) / ((2*i+1)*(pow(x+1, 2)));
+		printf_s("F%d = %.8lf\n", i, F * -1);
+		sum += F;
+	
+	}
+
+	printf_s("Sum = %.8lf\n", sum*-1);
+}
+
+
+
