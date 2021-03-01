fefd167854691b51722729c44da53ab62500b584
Maksym <71383659+Maksolo27@users.noreply.github.com>
2021-03-01 18:16:01 +0200

    Add files via upload

--- ASD_LABS/ASD_LAB_6/ConsoleApplication14.cpp
+++ ASD_LABS/ASD_LAB_6/ConsoleApplication14.cpp
@@ -1,12 +1,12 @@
 ﻿#include <stdio.h>
 #include <math.h>
-#define x 0.1
+#define x 0.5
 
 
 float RecurseSum(int length, float prevF, float sum, int i) {
 	i++;
+	prevF = prevF * (2 * i - 1)* pow((x-1), 2) / ((2 * i + 1) * pow((x + 1), 2));
 	sum += prevF;
-	prevF = prevF * (2 * i - 1)* powf((x-1), 2) / ((2 * i + 1) * powf((x + 1), 2));
 	if (length + 1 != i) {
 		sum = RecurseSum(length, prevF, sum, i);
 	}
@@ -16,9 +16,9 @@ int main()
 {
 	float F = (x-1)/(x+1);
 	int input;
-	float sum = 0;
-	printf_s("Please, input your integer number\n");
+	float sum = (x-1)/(x+1);
+	printf_s("Enter your number: \n");
 	scanf_s("%d", &input);
 	sum = RecurseSum(input, F, sum, 1);
-	printf_s("Sum of range equal: ( %f )\n", sum);
+	printf_s("Sum =  %f \n", sum * -1);
 }
