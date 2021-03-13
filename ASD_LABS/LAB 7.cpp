e6685b056ae4f0af1b2485d4fdc6a7885a250ae4
Maksym <71383659+Maksolo27@users.noreply.github.com>
2021-03-13 21:43:40 +0300

    Add files via upload

---
+++ ASD_LABS/LAB 7.cpp
@@ -0,0 +1,61 @@
+﻿
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+typedef struct Node {
+    int value;
+    struct Node* next;
+} Node;
+
+
+void push(Node** head, int data) {
+    Node* tmp = (Node*)malloc(sizeof(Node));
+    tmp->value = data;
+    tmp->next = (*head);
+    (*head) = tmp;
+}
+
+
+int pop(Node** head) {
+    Node* prev = NULL;
+    int val;
+    if (head == NULL) {
+        exit(-1);
+    }
+    prev = (*head);
+    val = prev->value;
+    (*head) = (*head)->next;
+    free(prev);
+    return val;
+}
+Node* getLast(Node* head) {
+    if (head == NULL) {
+        return NULL;
+    }
+    while (head->next) {
+        head = head->next;
+    }
+    return head;
+}
+
+void pushBack(Node* head, int value) {
+    Node* last = getLast(head);
+    Node* tmp = (Node*)malloc(sizeof(Node));
+    tmp->value = value;
+    tmp->next = NULL;
+    last->next = tmp;
+}
+
+Node* getNth(Node* head, int n) {
+    int counter = 0;
+    while (counter < n && head) {
+        head = head->next;
+        counter++;
+    }
+    return head;
+}
+
+int main() {
+
+}
